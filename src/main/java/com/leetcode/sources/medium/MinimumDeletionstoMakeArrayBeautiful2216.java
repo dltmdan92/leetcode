4c9562ab085d014f3ec8c6d2744d2de07eef4a4f
dltmdan92 <dltmdan92@naver.com>
2023-05-04 01:37:13 +0900

    [medium] MinimumDeletionstoMakeArrayBeautiful2216

--- src/main/java/com/leetcode/sources/medium/MinimumDeletionstoMakeArrayBeautiful2216.java
+++ src/main/java/com/leetcode/sources/medium/MinimumDeletionstoMakeArrayBeautiful2216.java
@@ -3,14 +3,14 @@ package com.leetcode.sources.medium;
 public class MinimumDeletionstoMakeArrayBeautiful2216 {
 
     public static void main(String[] args) {
-        System.out.println(minDeletion(new int[] {1,1,2,3,5}));
+        System.out.println(minDeletion(new int[] {1,1,2,2,3,3}));
     }
 
     public static int minDeletion(int[] nums) {
         int deleteCount = 0;
         int i = 0;
 
-        while (i < nums.length - 1) {
+        while (i <= nums.length - 1) {
             int comparePlusIndex = 1;
 
             while (i + comparePlusIndex < nums.length && nums[i] == nums[i + comparePlusIndex]) {
@@ -18,7 +18,7 @@ public class MinimumDeletionstoMakeArrayBeautiful2216 {
                 comparePlusIndex++;
             }
             i += comparePlusIndex + 1;
-            if (i >= nums.length) {
+            if (i > nums.length) {
                 deleteCount++;
             }
         }
