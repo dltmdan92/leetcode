a4904afe40383cffc8a364b15b76aa184c170751
dltmdan92 <dltmdan92@naver.com>
2023-05-04 01:36:03 +0900

    [medium] MinimumDeletionstoMakeArrayBeautiful2216

---
+++ src/main/java/com/leetcode/sources/medium/MinimumDeletionstoMakeArrayBeautiful2216.java
@@ -0,0 +1,28 @@
+package com.leetcode.sources.medium;
+
+public class MinimumDeletionstoMakeArrayBeautiful2216 {
+
+    public static void main(String[] args) {
+        System.out.println(minDeletion(new int[] {1,1,2,3,5}));
+    }
+
+    public static int minDeletion(int[] nums) {
+        int deleteCount = 0;
+        int i = 0;
+
+        while (i < nums.length - 1) {
+            int comparePlusIndex = 1;
+
+            while (i + comparePlusIndex < nums.length && nums[i] == nums[i + comparePlusIndex]) {
+                deleteCount++;
+                comparePlusIndex++;
+            }
+            i += comparePlusIndex + 1;
+            if (i >= nums.length) {
+                deleteCount++;
+            }
+        }
+        return deleteCount;
+    }
+
+}
