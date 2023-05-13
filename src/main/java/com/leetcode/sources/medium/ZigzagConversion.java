16075e503cb9eaa24ce649aa88383c995c6bcb8d
dltmdan92 <dltmdan92@naver.com>
2023-05-14 00:14:24 +0900

    [medium] 지그재그 스트링 통과

--- src/main/java/com/leetcode/sources/medium/ZigzagConversion.java
+++ src/main/java/com/leetcode/sources/medium/ZigzagConversion.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class ZigzagConversion {
 
     public static void main(String[] args) {
-        System.out.println(result("PAYPALISHIRING", 4));
+        System.out.println(result("PAYPALISHIRING", 5));
     }
 
     private static String result(String s, int numRows) {
@@ -47,21 +47,53 @@ public class ZigzagConversion {
         zigzagLineRow -> verticalLineRow - 2
          */
 
-        char[] c = s.toCharArray();
-        int len = c.length;
-        StringBuffer[] sb = new StringBuffer[numRows];
-        for (int i = 0; i < sb.length; i++) sb[i] = new StringBuffer();
+        int point = 0;
+        List<List<Character>> list = new ArrayList<>();
 
-        int i = 0;
-        while (i < len) {
-            for (int idx = 0; idx < numRows && i < len; idx++) // vertically down
-                sb[idx].append(c[i++]);
-            for (int idx = numRows-2; idx >= 1 && i < len; idx--) // obliquely up
-                sb[idx].append(c[i++]);
+        while (point < s.length()) {
+            List<Character> firstList = new ArrayList<>();
+            for (int i = 0; i < numRows & point < s.length(); i++) {
+                firstList.add(s.charAt(point));
+                point++;
+            }
+            int firstListTODO = numRows - firstList.size();
+            for (int i = 0; i < firstListTODO; i++) {
+                firstList.add(Character.MIN_VALUE);
+            }
+
+            List<Character> secondList = new ArrayList<>();
+            for (int i = 0; i < numRows - 2 & point < s.length(); i++) {
+                secondList.add(s.charAt(point));
+                point++;
+            }
+            secondList.add(0, Character.MIN_VALUE);
+            int secondListTODO = numRows - secondList.size();
+            for (int i = 0; i < secondListTODO; i++) {
+                secondList.add(Character.MIN_VALUE);
+            }
+            Collections.reverse(secondList);
+            list.add(firstList);
+            list.add(secondList);
         }
-        for (int idx = 1; idx < sb.length; idx++)
-            sb[0].append(sb[idx]);
-        return sb[0].toString();
+
+        /*
+        PAYPA
+        0LIS0
+        HIRIN
+        000G0
+         */
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < numRows; i++) {
+            for (List<Character> characters : list) {
+                Character character = characters.get(i);
+                if (character != 0) {
+                    sb.append(character);
+                }
+            }
+        }
+        return sb.toString();
     }
 
 }
